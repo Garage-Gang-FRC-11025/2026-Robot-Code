6f75289fbd67052a5884cd69bf38133f0d058f63
garage-gang-11025 <garagegang@gallatinvalleyrobotics.org>
2026-01-31 12:46:55 -0800

    Intake Code

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,5 +1,20 @@
 package frc.robot.subsystems.Intake;
 
-public class Intake {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class Intake extends SubsystemBase {
+
+    private final IntakeIO intakeIO;
+
+    public Intake(IntakeIO intakeIO)   {
+    this.intakeIO = intakeIO;
+
+    }
+
+
+    public void periodic() {
+
+
+
+    }
 }
