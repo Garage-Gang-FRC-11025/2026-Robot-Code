5964f133702daab9005186b9b441fa594adc91a4
GarageGang <garagegang@gallatinvalleyrobotics.org>
2026-02-19 20:12:22 -0700

    Fixed Duplication

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -25,7 +25,6 @@ public interface ShooterIO {
     public double rotationCurrentAmps;
     public double rotationTempCelsius;
     public Rotation2d rotationPosition;
-    public double rotationVelocity;
   }
 
   public default void updateInputs(shooterInputs inputs) {}
