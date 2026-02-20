d1d4046d57c9c57678138c2c4d1161da61963c59
Shy-Bry <brytondurham09@gmail.com>
2026-02-19 17:30:32 -0700

    Constants for systems partialy implmented

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -74,27 +74,40 @@ public final class Constants {
     public static final int INTAKE_EXTENDER_CAN_ID = 9;
     public static final int INTAKE_ROLLER_CAN_ID = 10;
     public static final int ELEVATOR_ROLLER_CAN_ID = 11;
-    public static final int ELEVATOR_ELEVATOR_CAN_ID = 12;
     public static final int ELEVATOR_CAN_ID = 12;
-  }
+    public static final double MAX_VOLTAGE = 12.0;
+    public static final double kDefaultPeriod = 0.02;
+    public static final int SHOOTER_WHEEL_CAN_ID = 12;
+    public static final int SHOOTER_HOOD_CAN_ID = 13;
+    public static final int SHOOTER_ROTATION_CAN_ID = 14;
+    }
 
-  
-  public static final double MAX_VOLTAGE = 12.0;
-  public static final double kDefaultPeriod = 0.02;
   public final class ShooterConstants {
     public static final double GEAR_RATIO = 1.0;
 
-    public final class TurretConstants {
-      public static final Rotation2d HORIZONTAL_MIN_ANGLE = Rotation2d.fromDegrees(0);
-      public static final Rotation2d HORIZONTAL_MAX_ANGLE = Rotation2d.fromDegrees(0);
-      public static final Rotation2d VERTICAL_MIN_ANGLE = Rotation2d.fromDegrees(0);
-      public static final Rotation2d VERTICAL_MAX_ANGLE = Rotation2d.fromDegrees(0);
+    public final class HoodConstants {
+      public static final Rotation2d MIN_HOOD_ANGLE = Rotation2d.fromDegrees(0);
+      public static final Rotation2d MAX_HOOD_ANGLE = Rotation2d.fromDegrees(90);
+      public static final double STATOR_CURRENT_LIMIT = 20;
+      public static final double SUPPLY_CURRENT_LIMIT = 20;
+      public static final double MAX_VELOCITY = 10;
+      public static final double TARGET_ACCELERATION = 10;
+      public static final double GEAR_RATIO = 1.0;
+      public static final double SUPPLY_VOLTAGE_TIME = 0.5;
     }
 
-    public class CanIDs {
-      public static final int HORIZONTAL_ANGLE_MOTOR_CAN_ID = 6;
-      public static final int VERTICAL_ANGLE_MOTOR_CAN_ID = 7;
-      public static final int SHOOTER_MOTOR_CAN_ID = 8;
+    public final class RotationConstants {
+      public static final Rotation2d ROTATION_MIN_ANGLE = Rotation2d.fromDegrees(0);
+      public static final Rotation2d ROTATION_MAX_ANGLE = Rotation2d.fromDegrees(180);
     }
+
+        public final class WheelConstants {
+      public static final double WHEEL_RADIUS_METERS = 0.0762; // 3 inches
+      public static final double WHEEL_GEARING = 1.0;
+      public static final double WHEEL_MOI = 0.1;
+      public static final double SUPPLY_CURRENT_LIMIT = 40;
+      public static final double STATOR_CURRENT_LIMIT = 40;
+      public static final double SUPPLY_VOLTAGE_TIME = 0.5;
+        }
   }
 }
