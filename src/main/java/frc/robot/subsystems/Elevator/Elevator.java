f32edcd078995b5bf1525c5da3d66be82d01b27c
Shy-Bry <brytondurham09@gmail.com>
2026-02-25 20:44:38 -0700

    Added needed dependents

---
+++ src/main/java/frc/robot/subsystems/Elevator/Elevator.java
@@ -0,0 +1,68 @@
+package frc.robot.subsystems.Elevator;
+
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.Mode;
+import frc.robot.util.LoggedTunableNumber;
+import org.littletonrobotics.junction.Logger;
+
+public class Elevator extends SubsystemBase {
+
+  private final ElevatorIO ElevatorIO;
+  private final ElevatorInputsAutoLogged inputs = new ElevatorInputsAutoLogged();
+
+  private static final LoggedTunableNumber eleKP = new LoggedTunableNumber("Elevator/kP");
+  private static final LoggedTunableNumber eleKV = new LoggedTunableNumber("Elevator/kV");
+  private static final LoggedTunableNumber elevatorTargetAccelerationConfig =
+      new LoggedTunableNumber("Elevator/Acceleration");
+  private static final LoggedTunableNumber elevatorMaxVelocityConfig =
+      new LoggedTunableNumber("Elevator/MaxVelocity");
+
+  static {
+    if (Constants.currentMode == Mode.REAL) {
+      eleKP.initDefault(0.8);
+      eleKV.initDefault(0.15);
+
+      elevatorTargetAccelerationConfig.initDefault(300.0);
+      elevatorMaxVelocityConfig.initDefault(1500.0);
+    } else {
+      eleKP.initDefault(1);
+      eleKV.initDefault(1);
+
+      elevatorTargetAccelerationConfig.initDefault(500.0);
+      elevatorMaxVelocityConfig.initDefault(2000.0);
+    }
+  }
+
+  public Elevator(ElevatorIO ElevatorIO) {
+    this.ElevatorIO = ElevatorIO;
+    configElevator();
+  }
+
+  public void periodic() {
+    Logger.processInputs("Elevator", inputs);
+    int hc = hashCode();
+    if (eleKP.hasChanged(hc)
+        || eleKV.hasChanged(hc)
+        || elevatorMaxVelocityConfig.hasChanged(hc)
+        || elevatorTargetAccelerationConfig.hasChanged(hc)) configElevator();
+    ElevatorIO.updateInputs(inputs);
+  }
+
+  public void setElevatorVoltage(double volts) {
+    ElevatorIO.setElevatorVoltage(volts);
+  }
+
+  public void setElevatorVel(AngularVelocity vel) {
+    ElevatorIO.setElevatorVel(vel);
+  }
+
+  private void configElevator() {
+    MotionMagicConfigs mmConfigs = new MotionMagicConfigs();
+    mmConfigs.MotionMagicAcceleration = elevatorTargetAccelerationConfig.get();
+    mmConfigs.MotionMagicCruiseVelocity = elevatorMaxVelocityConfig.get();
+    ElevatorIO.configElevator(eleKP.get(), eleKV.get(), elevatorMaxVelocityConfig.get());
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/Elevator/ElevatorIOSim.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems.Elevator;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.AngularVelocity;
+import frc.robot.Constants;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.util.TalonFXSim;
+
+public class ElevatorIOSim implements ElevatorIO {
+
+  private TalonFXSim elevatorMotor =
+      new TalonFXSim(
+          DCMotor.getKrakenX60Foc(1),
+          ElevatorConstants.ELEVATOR_GEARING,
+          ElevatorConstants.ELEVATOR_MOI);
+
+  private VoltageOut elevatorOpenLoopControl = new VoltageOut(0);
+  private VelocityVoltage elevatorClosedLoopControl = new VelocityVoltage(0);
+
+  public ElevatorIOSim() {}
+
+  @Override
+  public void updateInputs(ElevatorInputs inputs) {
+    elevatorMotor.update(Constants.kDefaultPeriod);
+    inputs.elevatorsAppliedOutput = elevatorMotor.getVoltage();
+    inputs.elevatorsVelocityRPM = elevatorMotor.getVelocity().in(Units.RPM);
+  }
+
+  @Override
+  public void setElevatorVoltage(double volts) {
+    elevatorMotor.setControl(elevatorOpenLoopControl.withOutput(volts));
+  }
+
+  @Override
+  public void setElevatorVel(AngularVelocity revPerMin) {
+    elevatorMotor.setControl(elevatorClosedLoopControl.withVelocity(revPerMin));
+  }
+
+  public void configElevators(double kV, double kP, double maxAcceleration) {
+    TalonFXConfiguration config = new TalonFXConfiguration();
+    Slot0Configs slot0Configs = new Slot0Configs();
+
+    slot0Configs.kP = kP;
+    slot0Configs.kV = kV;
+
+    config.Slot0 = slot0Configs;
+
+    elevatorMotor.setConfig(config);
+  }
+}
