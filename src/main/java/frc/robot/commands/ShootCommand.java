1a977a7de7694410f839907c0a2b4dc99a68d79f
Rodrick Bartholomew Elizabeth the Second <gg@gallatinvalleyrobotics.org>
2026-03-26 21:18:11 -0700

    Added Simple Shoot

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -8,19 +8,23 @@ import frc.robot.util.LoggedTunableNumber;
 public class ShootCommand extends Command {
   private Elevator elevator;
   private PrimeShootCommand primeShootCommand;
-
+  private PrimeShootCommand simplePrimeShootCommand;
   private static final LoggedTunableNumber elevatorVelocityConfig =
       new LoggedTunableNumber("Elevator/Elevator/Velocity", 400);
 
-  public ShootCommand(Elevator elevator, PrimeShootCommand primeShootCommand) {
+  public ShootCommand(
+      Elevator elevator,
+      PrimeShootCommand primeShootCommand,
+      PrimeShootCommand simplePrimeShootCommand) {
     this.elevator = elevator;
     this.primeShootCommand = primeShootCommand;
+    this.simplePrimeShootCommand = simplePrimeShootCommand;
     addRequirements(elevator);
   }
 
   @Override
   public void execute() {
-    if (primeShootCommand.isPrimed())
+    if (primeShootCommand.isPrimed() || simplePrimeShootCommand.isPrimed())
       elevator.setElevatorVel(Units.RPM.of(elevatorVelocityConfig.get()));
   }
 
